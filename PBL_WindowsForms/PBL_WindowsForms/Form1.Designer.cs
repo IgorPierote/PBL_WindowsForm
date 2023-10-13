c4e445a813bf6205ceccd263b5fdb5b60bb5da56
Igor A. Pierote <113553900+IgorPierote@users.noreply.github.com>
2023-10-13 16:02:06 -0300

    Add files via upload

---
+++ PBL_WindowsForms/PBL_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PBL_WindowsForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBoxAngulo = new System.Windows.Forms.TextBox();
+            this.textBoxVelocidade = new System.Windows.Forms.TextBox();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxAngulo
+            // 
+            this.textBoxAngulo.Location = new System.Drawing.Point(12, 12);
+            this.textBoxAngulo.Name = "textBoxAngulo";
+            this.textBoxAngulo.Size = new System.Drawing.Size(97, 22);
+            this.textBoxAngulo.TabIndex = 0;
+            // 
+            // textBoxVelocidade
+            // 
+            this.textBoxVelocidade.Location = new System.Drawing.Point(130, 12);
+            this.textBoxVelocidade.Name = "textBoxVelocidade";
+            this.textBoxVelocidade.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVelocidade.TabIndex = 1;
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.Location = new System.Drawing.Point(257, 12);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalcular.TabIndex = 2;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click_1);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Location = new System.Drawing.Point(360, 15);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(69, 16);
+            this.labelResultado.TabIndex = 3;
+            this.labelResultado.Text = "Resultado";
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(12, 138);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(776, 300);
+            this.chart.TabIndex = 4;
+            this.chart.Text = "chart1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.textBoxVelocidade);
+            this.Controls.Add(this.textBoxAngulo);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAngulo;
+        private System.Windows.Forms.TextBox textBoxVelocidade;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ PBL_WindowsForms/PBL_WindowsForms/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using PBL_WindowsForms.Objetos;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PBL_WindowsForms
+
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            chart.Parent = this;
+            chart.Location = new System.Drawing.Point(10, 150); // Ajuste a localização conforme necessário
+            chart.Size = new System.Drawing.Size(400, 300); // Ajuste o tamanho conforme necessário
+
+            // Adicione séries para o projétil e o meteoro
+            Series seriesProjetil = new Series("Projétil");
+            Series seriesMeteoro = new Series("Meteoro");
+            seriesProjetil.ChartType = SeriesChartType.Line;
+            seriesMeteoro.ChartType = SeriesChartType.Line;
+            chart.Series.Add(seriesProjetil);
+            chart.Series.Add(seriesMeteoro);
+
+        }
+
+        private void buttonCalcular_Click_1(object sender, EventArgs e)
+        {
+            double velocidadeInicial;
+            double anguloDaTrajetoria;
+
+            if (!double.TryParse(textBoxAngulo.Text, out anguloDaTrajetoria) || !double.TryParse(textBoxVelocidade.Text, out velocidadeInicial))
+            {
+                MessageBox.Show("Valores inválidos. Certifique-se de inserir números válidos.");
+                return;
+            }
+
+            Projetil projetil = new Projetil(velocidadeInicial, anguloDaTrajetoria);
+            Meteoro meteoro = new Meteoro();
+            double tempo = meteoro.DistanciaDoCanhao / projetil.Vox;
+            meteoro.SetAltura(tempo);
+            projetil.SetAltura(tempo);
+
+            if (projetil.Altura < 0)
+            {
+                labelResultado.Text = "Infelizmente o projétil não foi capaz de atingir o alvo";
+            }
+            else if (projetil.Altura == meteoro.Altura)
+            {
+                labelResultado.Text = $"O projétil atingiu o alvo no ponto x:{meteoro.DistanciaDoCanhao} y:{meteoro.Altura}.\n" +
+                    $"O alvo foi acertado no instante {tempo}";
+                if (projetil.AlturaMaximaProjetil > projetil.Altura)
+                {
+                    labelResultado.Text += "\nO alvo foi acertado na trajetória descendente";
+                }
+                else
+                {
+                    labelResultado.Text += "\nO alvo foi acertado na trajetória ascendente";
+                }
+            }
+            else
+            {
+                labelResultado.Text = "Infelizmente o projétil não foi capaz de atingir o alvo";
+            }
+
+            for (double tempoInicial = 0; tempoInicial <= tempo; tempoInicial += 1)
+            {
+                meteoro.SetAltura(tempo);
+                projetil.SetAltura(tempo);
+
+                chart.Series["Projétil"].Points.AddXY(tempo, projetil.Altura);
+                chart.Series["Meteoro"].Points.AddXY(tempo, meteoro.Altura);
+            }
+        }
+    }
+}
